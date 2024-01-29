88a09b26205e434628f07a87fee5afad67fdb810
Vũ Kiên Trường <91974329+KTruong9303@users.noreply.github.com>
2024-01-29 22:37:53 +0700

    Update client thêm asyncstorage

--- App/App.js
+++ App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState, useRef } from "react";
 import { io } from "socket.io-client";
 import {
   Text,
@@ -9,23 +9,23 @@ import {
   TextInput,
   Button,
   TouchableOpacity,
-  FlatList,
   ScrollView,
+  Alert
 } from "react-native";
-import { SliderTrack, Stack, stackClasses } from "@mui/material";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // import TicTacToe from "./TicTacToeBoard";
 
 
 export default function App() {
-  const socket = useMemo(() => io("http://192.168.1.121:3000"), []);
-  
+  const socket = useMemo(() => io("http://192.168.1.7:3000"), []);
+
   const [message, setMessage] = useState("");
   const [room, setRoom] = useState("");
   const [socketId, setSocketId] = useState("");
-  const [messages, setMessages] = useState([]);
   const [roomname, setRoomname] = useState("");
   const [game, setGame] = useState({ board: Array(9).fill(null), players: [], currentPlayer: '' });
-
+  const [history, setHistory] = useState([]);
+  const [receive,setReceive] = useState([]);
 
   const createGameHandler = () => {
     socket.emit('create-game', room);
@@ -54,20 +54,103 @@ export default function App() {
     );
   };
 
+  const getMessages = async () => {
+      try {
+        // Lấy danh sách tin nhắn từ AsyncStorage
+        
+        const messages_his = await AsyncStorage.getItem(roomname);
+        
+        if (messages_his) {
+          // If messages_his is not null or undefined, parse it
+          setHistory(JSON.parse(messages_his));
+          return JSON.parse(messages_his);
+        } else {
+          // If messages_his is null or undefined, return an empty array or another default value
+          setHistory(['Room name: ' + roomname, 'Hay bat dau cuoc tro chuyen!']);
+          return [];
+        }
+        
+      } catch (error) {
+        console.error('Lỗi khi lấy tin nhắn:', error);
+        return [];
+      }
+    };
+  const getRoomName = async () => {
+      console.log('Ten phong:   '+ roomname);
+      console.log(typeof(roomname));
+      
+      return roomname;
+  }
+  const storeMessage = async (content,rname) => {
+      try {
+        // Lấy danh sách tin nhắn đã lưu trước đó
+        const existingMessages = await AsyncStorage.getItem(rname);
+        
+        if (existingMessages) {
+           newMessages = JSON.parse(existingMessages);
+           newMessages.push(content);
+        }
+        else
+        {
+           newMessages = ['Room name: ' + rname];
+        }
+
+        // Lưu danh sách tin nhắn mới vào AsyncStorage
+        await AsyncStorage.setItem(rname, JSON.stringify(newMessages));
+
+      } catch (error) {
+        console.error('Lỗi khi lưu trữ tin nhắn:', error);
+
+      }
+    };
+  const scrollViewRef = useRef();
+  const handleContentSizeChange = () => {
+    if (scrollViewRef.current) {
+      scrollViewRef.current.scrollToEnd({ animated: true });
+    }
+  };
 
 
   const handleSubmit = () => {
+    //
+    //
     socket.emit("message",{message, room});
+    storeMessage(message,roomname).then(() => {getMessages();});
     setMessage("");
   };
 
   const joinRoomHandler = () => {
     socket.emit("join-room",roomname);
     socket.emit('join-game', roomname);
-    setRoomname("");
+    getMessages();
+
+    //setRoomname("");
   }
 
+const removeHistoryHandler = () => {
+  Alert.alert(
+    'Xác nhận xóa lịch sử tin nhắn',
+    'Bạn có chắc chắn muốn xóa cuộc hội thoại này?',
+    [
+      { text: 'Hủy', style: 'cancel' },
+      {
+        text: 'Xóa',
+        onPress: async () => {
+          try {
+            // Xử lý logic xóa tin nhắn ở đây
+            await AsyncStorage.removeItem(roomname);
+            console.log('Tin nhắn đã bị xóa');
+          } catch (error) {
+            console.error('Lỗi khi xóa tin nhắn:', error);
+          }
+        },
+      },
+    ],
+    { cancelable: false }
+  );
+};
 
+  
   useEffect(() => {
     socket.on("connect", () => {
       setSocketId(socket.id);
@@ -85,9 +168,9 @@ export default function App() {
 
 
     socket.on("receive-message", (data) => {
-      console.log(data);
-      setMessages([...messages, data]);
+      setReceive(...receive,data);
     });
+    
     socket.on("welcome", (s) => {
       console.log(s);
     });
@@ -95,14 +178,58 @@ export default function App() {
       socket.disconnect();
     };
   }, []);
+  
+    
+    
+////
+  useEffect(() => {
+      const fetchData = async () => {
+        if (roomname) {
+        await storeMessage('-> ' + receive, roomname);
+        await getMessages();
+        if (receive.length > 0) {
+          setReceive([]);
+        }
+        }
+      };
 
+      fetchData();
+    }, [receive, roomname]);
 
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
       <Text style={styles.text}>Socket ID: {socketId}</Text>
+      
       <TextInput
+        style={styles.inputStyle}
+        value={roomname}
+        placeholder="Room"
+        onChangeText={(text) => setRoomname(text)}
+      />
+      <Button title="Create/Join Room" onPress={joinRoomHandler} />
+      <Text style={styles.text}>Messages</Text>
+      
+      <ScrollView style={styles.scrollView}
+      ref={scrollViewRef}
+      onContentSizeChange={handleContentSizeChange}>
+        <View style={styles.container}>
+
+          {history.map((item, index) => (
+            <View key={index}>
+        <Text style={item.startsWith('->') ? styles.indentedText : styles.normalText}>
+          {item}
+        </Text>
+      </View>
+          ))}
+
+        </View>
+        </ScrollView>
+           
+      
+      
+    <TextInput
         style={styles.inputStyle}
         value={message}
         placeholder="Message"
@@ -116,24 +243,10 @@ export default function App() {
 
       />
       <Button title="Send" onPress={handleSubmit} />
-      <TextInput
-        style={styles.inputStyle}
-        value={roomname}
-        placeholder="Room"
-        onChangeText={(text) => setRoomname(text)}
-      />
-      <Button title="Create/Join Room" onPress={joinRoomHandler} />
-      <ScrollView>
-      {messages.map((m, i) => (
-        <View key={i} style={styles.messageContainer}>
-          <Text style={styles.messageText}>
-            {m}
-          </Text>
-        </View>
-      ))}
-    </ScrollView>
-
-
+      <Button title="Remove History" onPress={removeHistoryHandler} />
+        
+      {/* <Button title="Create Game" onPress={createGameHandler} />
+      {renderBoard()} */}
       
     </SafeAreaView>
   );
@@ -169,13 +282,17 @@ const styles = StyleSheet.create({
   cellText: {
     fontSize: 48,
   },
-  messageContainer: {
-    borderWidth: 1,
-    borderRadius: 6,
-    padding: 10,
-    marginVertical: 10,
+  normalText: {
+    marginBottom: 10, 
   },
-  messageText: {
-    fontSize: 18,
+  indentedText: {
+    marginLeft: 0, 
+    color: 'blue',
+    marginBottom: 10,
+  },
+  scrollView: {
+    flex: 1,
+    flexDirection: 'column',
+
   },
 });
